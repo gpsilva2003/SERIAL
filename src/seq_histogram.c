62a9b15a5596bdc5691700f76863b0ea747894e9
Gabriel P. Silva <gabriel.silva@gmail.com>
2025-11-17 21:23:40 -0300

    Refactor seq_histogram to use random data generation
    Updated the seq_histogram.c to generate random values and compute the histogram based on those values instead of user input.

--- src/seq_histogram.c
+++ src/seq_histogram.c
@@ -1,40 +1,65 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
-void printHistogram ( int *hist, int n );
+void printHistogram(int *hist, int n);
 
-int main(int argc, char *argv[]) {   /* histogram_seq.c   */
-   int i, j;
-   int inputValue;
+int main(int argc, char *argv[]) {   /* seq_histogram.c */
+    int i, j;
 
-   printf("Input the amount of values: \n");  
-   scanf("%d", &inputValue);
-   int hist[inputValue];
+    // Gerar quantidade suficiente de dados
+    int inputValue = 800000000;
+    int *data = (int *)malloc(inputValue * sizeof(int));
 
-   printf("Input the values between 0 and 9 (separated by space): \n");
-   for (i = 0; i < inputValue; ++i) {
-        scanf("%d", &hist[i]);  
-   }
+    if (data == NULL) {
+        printf("Erro ao alocar memória!\n");
+        return 1;
+    }
+
+    // Inicializar gerador de números aleatórios
+    srand(time(NULL));
+
+    printf("Gerando %d valores aleatórios entre 0 e 9...\n", inputValue);
+
+    // Gerar valores aleatórios
+    for (i = 0; i < inputValue; i++) {
+        data[i] = rand() % 10;
+    }
+
+    printf("Valores gerados com sucesso!\n");
+
+    // Iniciar medição de tempo
+    clock_t start = clock();
 
     int results[10] = {0};
-    // Processing data to compute histogram, see 5.17
-    for (i = 0; i < 10; ++i) {   
-         for(j = 0; j < inputValue; j++) {
-             if (hist[j] == i) 
-                 results[i]++;
-         } 
+
+    // Processamento para computar histograma
+    for (i = 0; i < 10; ++i) {
+        for(j = 0; j < inputValue; j++) {
+            if (data[j] == i)
+                results[i]++;
+        }
     }
-    printf("\n");
+    // Finalizar medição de tempo
+    clock_t end = clock();
+    double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    printf("=== HISTOGRAMA ===\n");
     printHistogram(results, 10);
+
+    printf("\n=== MÉTRICAS DE DESEMPENHO ===\n");
+    printf("Tempo de execução: %.6f segundos\n", cpu_time_used);
+    printf("Total de valores processados: %d\n", inputValue);
+
+    // Liberar memória alocada
+    free(data);
+
     return 0;
 }
 
 void printHistogram(int *hist, int n) {
-      int i, j;
-      for (i = 0; i < n; i++) {
-           printf("[%d] ", i);
-           for (j = 0; j < hist[i]; ++j) {
-                printf("*");
-           }
-           printf("\n");
-      }
+    int i;
+    for (i = 0; i < n; i++) {
+            printf("hist[%d] = %d \n", i, hist[i]);
+        }
 }
