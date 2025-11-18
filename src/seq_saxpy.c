124b3b7b9d1160b2076a44e51f2f3a3a723f792c
Gabriel P. Silva <gabriel.silva@gmail.com>
2025-11-17 21:34:01 -0300

    Change saxpy function to use double precision

--- src/seq_saxpy.c
+++ src/seq_saxpy.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-void saxpy(int n, float a, float *x, float *restrict y)
+void saxpy(int n, double a, double *x, double *restrict y)
 {
 
 for (int i = 0; i < n; ++i)
@@ -11,14 +11,14 @@ for (int i = 0; i < n; ++i)
 
 int main(int argc, char **argv)
 {
-    int N = 1<<20; // 1 milhão de floats
+    long int N = 1<<30; // 1 bilhão de doubles
     clock_t inicio, fim;
     double tempo;
     if (argc > 1)
         N = atoi(argv[1]);
 
-    float *x = (float*)malloc(N * sizeof(float));
-    float *y = (float*)malloc(N * sizeof(float));
+    double *x = (double*)malloc(N * sizeof(double));
+    double *y = (double*)malloc(N * sizeof(double));
     for (int i = 0; i < N; ++i) {
         x[i] = 2.0f;
         y[i] = 1.0f;
@@ -28,8 +28,7 @@ int main(int argc, char **argv)
     fim = clock();
     tempo = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
     printf("Tempo de execução: %.6f segundos\n", tempo);
-    printf("Elementos: %lld — Taxa: %.2f milhões de elementos/seg\n",
-           n, (n / 1e6) / tempo);
+    printf("Elementos: %ld — Taxa: %.2f milhões de elementos/seg\n", N, (N / 1e6) / tempo);
     free(x);
     free(y);
     return 0;
